1a506b52d6b3ba7a5d4eb7be4e430fa524e69fc8
demetrigeras <demetrijgeras@gmail.com>
2023-11-21 12:06:47 -0500

    learning go

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
 }
